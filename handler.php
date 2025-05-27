d83d6826b7953357341e22e5392ba694e1a2ee87
joshuakiggundu <kiggundujoshua256@gmail.com>
2025-05-27 19:54:29 +0300

    first commit

--- handler.php
+++ handler.php
@@ -23,6 +23,6 @@ $pp->requireReCaptcha();
 $pp->getReCaptcha()->initSecretKey('6LdW03QgAAAAADVT9qY1gFgKKXY9KBeGG_5X7wbm');
 
 
-$pp->sendEmailTo('airoptionsuganda@gmail.com'); // ← Your email here
+$pp->sendEmailTo('info@airoptionsuganda.com'); // ← Your email here
 
 echo $pp->process($_POST);
